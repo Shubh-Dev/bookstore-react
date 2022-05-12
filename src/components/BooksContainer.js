7b110a652339fb7955e93983856df54a4477f504
Shubh <shubh.scb@gmail.com>
2022-05-12 21:31:13 +0530

    COMPONENT FILE ADD

---
+++ src/components/BooksContainer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Book = () => (
+  <div className="container">
+    <div className="inner">
+      <li>bookitem</li>
+    </div>
+  </div>
+);
+
+export default Book;
